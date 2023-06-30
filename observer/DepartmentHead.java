d587c4aec61cf1f90dedb114ccb92b31d4ddbba7
Alexey <besh1985@mail.ru>
2023-06-30 08:11:40 +0300

    HomeWork7

---
+++ observer/DepartmentHead.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.lesson7.observer;
+
+
+    public class DepartmentHead extends Worker{
+
+        public DepartmentHead(String name, String vacancy) {
+            this.name = name;
+            this.nameVacancy = vacancy;
+            minSalary = random.nextDouble(100000, 120000);
+            hasJob = false;
+        }
+
+        @Override
+        public String toString() {
+            return "Руководитель";
+        }
+    }
+
+
